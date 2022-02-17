bafe17bcf7b72988c6bfb7683a72b8db0a0868ce
albert.fang <albert.fang@51job.com>
2022-02-17 13:18:05 +0800

    sentinel 限流降级 & 熔断降级

--- 10/sentinel-sample/src/main/java/com/lagou/sentinelsample/controller/SentinelSampleController.java
+++ 10/sentinel-sample/src/main/java/com/lagou/sentinelsample/controller/SentinelSampleController.java
@@ -5,8 +5,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SentinelSampleController {
+
+    /**
+     * 限流降级
+     * @return
+     */
     @GetMapping("/test_flow_rule")
     public String testFlowRule(){
         return "SUCCESS";
+    }
+
+
+    /**
+     * 熔断降级：慢调用比例、异常数比例、异常数
+     * @return
+     */
+    @GetMapping("/test_fuse_rule")
+    public String testFuseRule(){
+        throw new RuntimeException("异常数");
     }
 }
